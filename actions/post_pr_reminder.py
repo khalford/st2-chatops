2fa91b3cabb90cf96eada3817dd817a23b59ad97
Kalibh Halford <kalibh.halford@stfc.ac.uk>
2024-04-08 10:37:54 +0100

    BUG: Stackstorm action fails to run
    The actions attempts to enter the incorrect class and therefore cannot find the "run" method. Moving the failing class to another file should mitigate this issue.

--- actions/post_pr_reminder.py
+++ actions/post_pr_reminder.py
@@ -4,40 +4,7 @@ import sys
 import json
 from st2common.runners.base_action import Action
 from slack_sdk import WebClient
-
-
-class GetMessages:
-
-    def __init__(self):
-        self.repos = self.get_repos()
-        self.secrets = self.get_secrets()
-
-    @staticmethod
-    def get_repos() -> List[str]:
-        with open("/etc/repos.csv", "r") as file:
-            data = file.read()
-            repos = data.split(".")
-        return repos
-
-    @staticmethod
-    def get_secrets() -> Dict[str,str]:
-        with open("/etc/st2_tokens.json", "r") as file:
-            data = file.read()
-            secrets = json.loads(data)
-        return secrets
-
-    def get_http_response_github(self, url: str) -> List[Dict]:
-        headers = {'Authorization': 'token ' + self.secrets['GITHUB_TOKEN']}
-        response = requests.get(url, headers=headers)
-        return response.json()
-
-    def get_raw_prs(self) -> List[List[Dict]]:
-        responses = []
-        for repo in self.repos:
-            url = f"https://api.github.com/repos/stfc/{repo}/pulls"
-            response = self.get_http_response_github(url)
-            responses.append(response)
-        return responses
+from get_messages import GetMessages
 
 
 class PostPRReminder(Action, GetMessages):
